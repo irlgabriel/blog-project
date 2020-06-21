9c8890fab9089309bccfe030146dc5b315e657ec
irlgabriel <radu.gabriel131@gmail.com>
2020-06-21 17:37:54 +0300

    used generate scaffold to generate all boilerplate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_130601) do
+ActiveRecord::Schema.define(version: 2020_06_21_143215) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
